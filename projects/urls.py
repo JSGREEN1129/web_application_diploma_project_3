ef5a4df9364f8e666882d9273575deaa159c648f
JSGREEN1129 <jsg12212@outlook.com>
2025-10-13 16:13:10 +0100

    Add favicon, create base templates and def project models, views and urls

---
+++ projects/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.project_list, name='project_list'),
+    path('create/', views.project_create, name='project_create'),
+    path('<int:project_id>/', views.project_detail, name='project_detail'),
+    path('<int:project_id>/edit/', views.project_edit, name='project_edit'),
+]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def project_list(request):
+    return render(request, "projects/project_list.html", context)
+
+def project_create(request):
+    return render(request, "projects/project_create.html", {"form": form})
+
+def project_detail(request, project_id):
+    return render(request, 'projects/project_detail.html', context)
