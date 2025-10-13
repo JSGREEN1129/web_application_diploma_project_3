9c6ddcd3050d4e49170bf1367043e882046f531f
JSGREEN1129 <jsg12212@outlook.com>
2025-10-13 17:21:53 +0100

    Added base and project templates and reset url routing and views

--- project_management_systems/urls.py
+++ project_management_systems/urls.py
@@ -1,10 +1,2 @@
 from django.contrib import admin
 from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('core.urls')),
-    path('projects/', include('projects.urls')),
-    path('tasks/', include('tasks.urls')),
-    path('users/', include('users.urls')),
-]
