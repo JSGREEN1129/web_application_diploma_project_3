7f7e7bbaf055471b426d82f7e94379c0fd374302
JSGREEN1129 <jsg12212@outlook.com>
2025-10-16 14:05:49 +0100

    route tasks app urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,2 +1,8 @@
 from django.urls import path
 from . import views
+
+urlpatterns = [
+    path('project/<int:project_id>/create/', views.task_create, name='task_create'),
+    path('tasks/<int:task_id>/', views.task_detail, name='task_detail'),
+    path('<int:task_id>/edit/', views.task_edit, name='task_edit'),
+]
