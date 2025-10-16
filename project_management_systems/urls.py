7dc2f6a89f9541ed26056e297df98f287b292968
JSGREEN1129 <jsg12212@outlook.com>
2025-10-16 14:23:35 +0100

    route urls for project edit, confrim delete and toggle complete

--- project_management_systems/urls.py
+++ project_management_systems/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('users/', include('users.urls')),
-    path('projects/', include('projects.urls'))
+    path('projects/', include('projects.urls')),
+    path('tasks/', include('tasks.urls'))
 ]
--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('create/', views.project_create, name='project_create'),
-    path('', views.project_list, name='project_list')
+    path('', views.project_list, name='project_list'),
+    path('<int:project_id>/', views.project_detail, name='project_detail'),
+    path('<int:project_id>/edit/', views.project_edit, name='project_edit'),
+    path('<int:project_id>/delete/', views.project_confirm_delete, name='project_confirm_delete'),
+    path('<int:project_id>/toggle_complete/', views.project_toggle_complete, name='project_toggle_complete'),
 ]
